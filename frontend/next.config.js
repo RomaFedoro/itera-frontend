7a5809967eba1925d6e617216479b07f639bd9fc
RomaFedoro <fedorovroman1506@gmail.com>
2023-02-16 22:22:36 +0700

    fix(font): remove Next Font

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,12 +3,6 @@ const nextConfig = {
   pageExtensions: ['ts', 'tsx'],
   experimental: {
     appDir: true,
-    fontLoaders: [
-      {
-        loader: '@next/font/google',
-        options: { subsets: ['latin', 'cyrillic'] },
-      },
-    ],
   },
   async redirects() {
     return [
