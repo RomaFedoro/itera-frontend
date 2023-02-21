958e7bb4f78a7d99866c13a17ede4cf180567551
RomaFedoro <fedorovroman1506@gmail.com>
2023-02-21 12:10:18 +0700

    feat(menu): add logo

--- frontend/src/container/Menu/types.ts
+++ frontend/src/container/Menu/types.ts
@@ -1,4 +1,7 @@
+import { ForwardRefExoticComponent, SVGProps } from 'react';
+
 export type TMenuLink = {
   link: string;
   title: string;
+  Icon?: ForwardRefExoticComponent<SVGProps<SVGSVGElement>>;
 };
