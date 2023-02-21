485af2ed1ef8407c5c976728e5a5b38503dc7fed
RomaFedoro <fedorovroman1506@gmail.com>
2023-02-21 10:01:17 +0700

    fix(habit): habit component

--- frontend/src/app/habits/[id]/page.tsx
+++ frontend/src/app/habits/[id]/page.tsx
@@ -1,30 +1,9 @@
-'use client';
-
-import HabitInfo from '@/components/habit/HabitInfo';
-import HabitStatistics from '@/components/habit/HabitStatistics';
-import { useQuery } from '@tanstack/react-query';
-import { getHabitFetch } from '@/services/habits';
+import Habit from '@/container/Habit';
 
 export default function HabitPage({
   params: { id },
 }: {
   params: { id: string };
 }) {
-  const {
-    isLoading,
-    isError,
-    data: habit,
-  } = useQuery({
-    queryKey: ['habits', id],
-    queryFn: () => getHabitFetch(id),
-  });
-
-  if (!habit) return null;
-
-  return (
-    <>
-      <HabitInfo {...habit.data} />
-      <HabitStatistics {...habit.data} />
-    </>
-  );
+  return <Habit id={id} />;
 }
